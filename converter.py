d3ac3c0832871de12778d38dbd6afa3580293b8c
Matvei2002 <mkozlov116@gmail.com>
2023-10-16 21:43:23 +0300

    First commit

--- converter.py
+++ converter.py
@@ -3,36 +3,41 @@ from tkinter import filedialog
 import docx2pdf
 import pdf2docx
 
-root = tk.Tk()
-root.title("Конвертер файлов")
-root.geometry('720x480')
-root.resizable(width=False, height=False)
-root['bg'] = '#808080'
+class DocumentConverter:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Конвертер файлов")
+        self.root.geometry('720x480')
+        self.root.resizable(width=False, height=False)
+        self.root['bg'] = '#808080'
+        
+        self.setup_ui()
 
+    def setup_ui(self):
+        self.pdf_to_docx_button = tk.Button(self.root, text="PDF to DOCX", font=('Arial', 20, 'bold'), fg='#FF0000', bg='#FFFFFF', command=self.pdf_to_docx)
+        self.pdf_to_docx_button.pack(pady=10)
 
-def pdf_to_docx():
-    file_path = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")])
-    if file_path:
-        docx_file = file_path[:-4] + ".docx"
-        pdf2docx.parse(file_path, docx_file)
-        success_label.config(text="Конвертация прошла успешно")
+        self.docx_to_pdf_button = tk.Button(self.root, text="DOCX to PDF", font=('Arial', 20, 'bold'), fg='#FF0000', bg='#FFFFFF', command=self.docx_to_pdf)
+        self.docx_to_pdf_button.pack(pady=10)
 
-def docx_to_pdf():
-    file_path = filedialog.askopenfilename(filetypes=[("Word Files", "*.docx")])
-    if file_path:
-        pdf_file = file_path[:-5] + ".pdf"
-        docx2pdf.convert(file_path, pdf_file)
-        success_label.config(text="Конвертация прошла успешно")
+        self.success_label = tk.Label(self.root, fg="red", bg="gray", font=('Arial', 20, 'bold'))
+        self.success_label.pack(pady=10)
 
-pdf_to_docx_button = tk.Button(root, text="PDF to DOCX", font='Arial 20 bold',
-       fg='#FF0000', bg='#FFFFFF', command=pdf_to_docx)
-pdf_to_docx_button.pack(pady=10)
+    def pdf_to_docx(self):
+        file_path = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")])
+        if file_path:
+            docx_file = file_path[:-4] + ".docx"
+            pdf2docx.convert(file_path, docx_file)
+            self.success_label.config(text="Конвертация из PDF в DOCX прошла успешно")
 
-docx_to_pdf_button = tk.Button(root, text="DOCX to PDF", font='Arial 20 bold',
-       fg='#FF0000', bg='#FFFFFF', command=docx_to_pdf)
-docx_to_pdf_button.pack(pady=10)
+    def docx_to_pdf(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Word Files", "*.docx")])
+        if file_path:
+            pdf_file = file_path[:-5] + ".pdf"
+            docx2pdf.convert(file_path, pdf_file)
+            self.success_label.config(text="Конвертация из DOCX в PDF прошла успешно")
 
-success_label = tk.Label(root, fg="red", bg="gray", font='Arial 20 bold')
-success_label.pack(pady=10)
-
-root.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = DocumentConverter(root)
+    root.mainloop()
